d05b76b8b3625eea2f340e648ea8092cc88d3eaf
Ton Nom <enochkuwonu10@gmail.com>
2026-04-14 20:19:11 +0200

    TP3 final - CRUD complets, données réelles, front-office et back-office fonctionnels

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\FiliereRepository;
+use App\Repository\EtablissementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,15 +11,22 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AdminDashboardController extends AbstractController
 {
     #[Route('/admin', name: 'admin_dashboard')]
-    public function index(): Response
+    public function index(
+        FiliereRepository $filiereRepository,
+        EtablissementRepository $etablissementRepository
+    ): Response
     {
+        $filieres = $filiereRepository->findAll();
+        $etablissements = $etablissementRepository->findAll();
+
         $stats = [
-            'filieres' => 3,
-            'etablissements' => 3,
+            'filieres' => count($filieres),
+            'etablissements' => count($etablissements),
         ];
 
         return $this->render('admin/dashboard.html.twig', [
             'stats' => $stats,
+            'filieres' => $filieres,
         ]);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\FiliereRepository;
+use App\Repository\EtablissementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,5 +14,32 @@ final class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('front/home.html.twig');
+    }
+
+    #[Route('/filieres', name: 'front_filiere_index')]
+    public function filieres(FiliereRepository $filiereRepository): Response
+    {
+        $filieres = $filiereRepository->findAll();
+        return $this->render('front/filiere/index.html.twig', [
+            'filieres' => $filieres,
+        ]);
+    }
+
+    #[Route('/filieres/{id}', name: 'front_filiere_show')]
+    public function filiereShow(int $id, FiliereRepository $filiereRepository): Response
+    {
+        $filiere = $filiereRepository->find($id);
+        return $this->render('front/filiere/show.html.twig', [
+            'filiere' => $filiere,
+        ]);
+    }
+
+    #[Route('/etablissements', name: 'front_etablissement_index')]
+    public function etablissements(EtablissementRepository $etablissementRepository): Response
+    {
+        $etablissements = $etablissementRepository->findAll();
+        return $this->render('front/etablissement/index.html.twig', [
+            'etablissements' => $etablissements,
+        ]);
     }
 }
